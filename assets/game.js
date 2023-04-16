f86599eef6c8e9e941a90394457c383153343057
francienevaz <fran.vfraga@gmail.com>
2023-04-16 17:19:40 -0300

    updated

--- assets/game.js
+++ assets/game.js
@@ -18,9 +18,8 @@ const loop = setInterval(() => {
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     const cloudsPosition = clouds.offsetLeft;
 
-    console.log(marioPosition);
-
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
